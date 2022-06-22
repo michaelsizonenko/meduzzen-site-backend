bfd3937449786a57429fc68fcb3bec6896158e4a
RomanPoplietukhin <romapopletuhin@gmail.com>
2022-06-22 20:23:07 +0300

    add email service

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ require('dotenv').config()
 app.use(bodyParser.json())
 app.use(formidable());
 app.use(bodyParser.urlencoded({extended: true}))
-
 app.use(function (req, res, next) {
 
     var origins = ['http://127.0.0.1', 'http://localhost'];
 
-    for (var i = 0; i < origins.length; i++) {
-        var origin = origins[i];
+    for (let i = 0; i < origins.length; i++) {
+        let origin = origins[i];
 
         if (req.headers.origin.indexOf(origin) > -1) {
             res.header('Access-Control-Allow-Origin', req.headers.origin);
@@ -36,14 +35,6 @@ const transporter = nodemailer.createTransport({
     },
 })
 
-
-const mailer = message => {
-    transporter.sendMail(message, (err, info) => {
-        //err && console.log(err)
-        //console.log(info)
-    })
-}
-
 app.post('/email', (req, resp) => {
 
     const fields = req.fields;
@@ -53,15 +44,17 @@ app.post('/email', (req, resp) => {
         from: process.env.SENDER,
         to: process.env.RECIPIENTS,
         html: `<h1>Letter from meduzzen!</h1>
-                <p> name: ${fields.name}</p>
-                <p> project type: ${fields.type} </p>
-                <p> number: ${fields.number}</p>
-                <p>email: ${fields.email}</p>
-                <p> about project: ${fields.about}</p>`,
+                <p> Name: ${fields.name}</p>
+                <p> Project type: ${fields.type} </p>
+                <p> Number: ${fields.number}</p>
+                <p> Email: ${fields.email}</p>
+                <p> About project: ${fields.about}</p>`,
         subject: `New order from meduzzen`,
         attachments: [{filename: files.image.name, path: files.image.path}]
     }
-    mailer(message)
+
+    transporter.sendMail(message)
+
     resp.status(200).json(req.body.text)
 })
 
