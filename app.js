37b2b2d7419b76476dfd463261d097a2931ce7a1
michaelsyzonenko <m.syzonenko@eqwiventures.com>
2022-06-29 15:45:28 +0300

    fix: remove checker

--- app.js
+++ app.js
@@ -8,22 +8,7 @@ require('dotenv').config()
 app.use(bodyParser.json())
 app.use(formidable());
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(function (req, res, next) {
 
-    var origins = ['http://127.0.0.1', 'http://localhost', 'http://18.196.199.182'];
-
-    for (let i = 0; i < origins.length; i++) {
-        let origin = origins[i];
-
-        if (req.headers.origin.indexOf(origin) > -1) {
-            res.header('Access-Control-Allow-Origin', req.headers.origin);
-        }
-    }
-
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -55,6 +40,10 @@ app.post('/email', (req, resp) => {
     transporter.sendMail(message)
 
     resp.status(200).json(req.body.text)
+})
+
+app.get('/hello', function(req, res) {
+    res.send('hello world');
 })
 
 app.listen(5000, () => {
